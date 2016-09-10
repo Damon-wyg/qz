f6f164e6747f0f915fc9c4f9c4a59f3b7a4fcdfa
one <wangyungan@baidu.com>
2016-09-10 23:28:15 +0800

    change db setting

--- qz_master/settings.py
+++ qz_master/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'learn_pro',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'qz_master.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qz',
+        'USER': 'root',
+        'PASSWORD': '816329',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'CONN_MAX_AGE': 600,
     }
 }
 
@@ -85,7 +91,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh_CN'
 
 TIME_ZONE = 'UTC'
 
